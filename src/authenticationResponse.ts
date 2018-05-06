d280ec7c5cefca96af1e510208646cc3fd79b8c4
Chethiya Abeysinghe <chethiya@gmail.com>
2018-05-06 11:43:33 +0530

    Deleting classes menat to represent permissions.

--- src/authenticationResponse.ts
+++
@@ -1,8 +0,0 @@
-class AuthenticationResponse {
-  private identity: Identity;
-  private permissions: Permissions;
-  constructor(identity: Identity, permissions: Permissions) {
-    this.identity = identity;
-    this.permissions = permissions;
-  }
-}
