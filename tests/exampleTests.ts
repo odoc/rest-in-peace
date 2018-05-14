22ab3d2b694ff5c8ac44bc533e3efbf5afb7ab02
Chethiya Abeysinghe <chethiya@gmail.com>
2018-05-14 17:58:01 +0530

    Unit test TODOs for example

---
+++ tests/exampleTests.ts
@@ -0,0 +1,33 @@
+/*
+// TODO
+
+Create
+ - create by a user - existing user/admin/non existant user
+ - create data errors - mandatory vs nested - errors and type mismatches
+ - create success response code
+ - creating same userid error
+ - create array error handling - duplicates in the array, empty array, isarray and data mismatches
+
+Get
+ - get all by normal user error
+ - get by admin error
+ - get all result
+ - get by some other user error
+ - get non extent user by valid user
+ - get all when no users
+
+ PUT
+  - put response code,
+  - put with invalid/partial data error
+  - put array error
+  - put non existnat id by an existing account error
+  - put non existant id by admin
+  - put by non user error
+
+ DELET
+  - delete by a non user/user and admin
+  - PUT/GET before/after delete
+
+ custom add call event
+  - mixing put updates and add call event partial update and testing with get
+*/
