dc5272180e794647f41eee81a7f72b8a95314211
Chethiya Abeysinghe <chethiya@gmail.com>
2018-06-04 12:44:23 +0530

    Build script

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,15 @@ export { ResourceHandler, Method, RepresentationPair } from './resourceHandler';
 
 export { ResourceRequest, Resource, ResourceId } from './resourceRequest';
 export { ResourceResponse } from './responses/resourceResponse';
-export { SuccessResponse } from './responses/successResponse';
-export { ClientErrorResponse } from './responses/clientErrorResponse';
-export { ServerErrorResponse } from './responses/serverErrorResponse';
+export {
+  SuccessResponse, SuccessHttpStatusCode
+} from './responses/successResponse';
+export {
+  ClientErrorResponse, ClientErrorHttpStatusCode
+} from './responses/clientErrorResponse';
+export {
+  ServerErrorResponse, ServerErrorHttpStatusCode
+} from './responses/serverErrorResponse';
 
 export { AuthHandler } from './authHandler';
 export { Identity } from './identity';
